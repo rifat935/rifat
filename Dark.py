c43c44e77487732bb860db14e52421dd85513b20
picchi24 <99118481+picchi24@users.noreply.github.com>
2022-03-31 10:10:16 +0600

    Update Dark.py

--- Dark.py
+++ Dark.py
@@ -118,39 +118,37 @@ def main_input():
      print ("")
      print ("") 
      print ("") 
-     print 50*'|-|' 
-     print ("\x1b[1;93mName--------: Ovi Islam") 
-     print 50*'|-|' 
-     print ("\x1b[1;96mHometown----: Lakshmifur Chittagong") 
-     print 50*'|-|' 
-     print ("\x1b[1;94mStudy-------: Inter fast year") 
-     print 50*'|-|' 
-     print ("\x1b[1;95mAge---------: 17") 
-     print 50*'-|' 
-     print ("\x1b[1;92mRelationship: Single") 
-     print 50*'|~|' 
+     print 50*'-' 
+     print ("\x1b[1;93mNAME--------: SHAWON MALLIK") 
+     print 50*'-' 
+     print ("\x1b[1;96mHOMETOWN----: RAJSAHI") 
+     print 50*'-' 
+     print ("\x1b[1;94mSTUDY-------: HONARS 1ST") 
+     print 50*'-' 
+     print ("\x1b[1;95mAGE---------: 20") 
+     print 50*'-' 
+     print ("\x1b[1;92mRELATIONSHIP: SINGLE") 
+     print 50*'~' 
      print ("")
      time.sleep(3.5)
-     print "\x1b[1;91mFill in correctly"
-     pilih_super()
-
+     
     if mx=='2':
      os.system ('clear')
      print logo
      print ("")
      print ("") 
      print ("") 
-     print 50*'|-|' 
+     print 50*'-' 
      print ("\x1b[1;93mName--------: Ovi Islam") 
-     print 50*'|-|' 
+     print 50*'-' 
      print ("\x1b[1;96mHometown----: Lakshmifur Chittagong") 
-     print 50*'|-|' 
+     print 50*'-' 
      print ("\x1b[1;94mStudy-------: Inter fast year") 
-     print 50*'|-|' 
+     print 50*'-' 
      print ("\x1b[1;95mAge---------: 17") 
-     print 50*'-|' 
+     print 50*'-' 
      print ("\x1b[1;92mRelationship: Single") 
-     print 50*'|~|' 
+     print 50*'~' 
      print ("")
      time.sleep(3.5) 
 
