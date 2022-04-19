c6ab79ebb60c17769e2f4355e8478c745f619ab6
Bd Crack <99118481+rifat935@users.noreply.github.com>
2022-04-19 21:32:00 +0600

    Update xxx-dec.py

--- xxx-dec.py
+++ xxx-dec.py
@@ -247,12 +247,13 @@ class __xxx__:
             self.__pler__()
         elif chi in ('1', '01'):
             os.system("clear")
-            print(logo)
+            print(logo) 
+            print ('')
             print("\033[1;91m\rUSE FLIGHT (airplane) MODE ON\033[1;96m")
-            print('══════════════════════════════════════════')
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             print('\033[1;36mTOTAL IDS : %s ' % len(self.id))
             print('\033[1;36mCRACKING STARTED.....')
-            print('══════════════════════════════════════════')
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             with sarfrazssb(max_workers=30) as ssbworld:
                 for zsb in self.id: # PSYCHO PICCHI BANGLADESH HACKER
                     try:
