5b816d25ea4a3421bd5de8e81f98d1c9e6f790c4
Bd Crack <99118481+rifat935@users.noreply.github.com>
2022-04-19 18:09:56 +0600

    Update xxx-dec.py

--- xxx-dec.py
+++ xxx-dec.py
@@ -149,7 +149,7 @@ class __xxx__:
             self.sarfrazx(id)
     def __metode__(self, user, __chi__, cebok):
         global ok,cp,loop
-        sys.stdout.write(f"\r\x1b[1;97m PICCHI / {loop}|{len(self.id)} ok- {len(ok)} / cp- {len(cp)} ")
+        sys.stdout.write(f"\r\x1b[1;97m[PSY-PICCHI] {loop} | {len(self.id)} ok-{len(ok)} / cp-{len(cp)} ")
         sys.stdout.flush()
         try:
             for pw in __chi__:
